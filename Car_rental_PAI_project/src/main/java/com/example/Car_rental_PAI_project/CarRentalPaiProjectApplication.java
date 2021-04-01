871dea6a5d411975945139437f2df5485da56b1d
Adam Wencka <adamwencka7@gmail.com>
2021-04-01 12:25:28 +0200

    Project init

---
+++ Car_rental_PAI_project/src/main/java/com/example/Car_rental_PAI_project/CarRentalPaiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Car_rental_PAI_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalPaiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalPaiProjectApplication.class, args);
+	}
+
+}
---
+++ Car_rental_PAI_project/src/test/java/com/example/Car_rental_PAI_project/CarRentalPaiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Car_rental_PAI_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalPaiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
